4bbedc5c2aa89e4adab16928e99c1a3c7e3d0f96
songsaemi <songsaemiye@naver.com>
2023-07-17 13:06:42 +0900

    초기 환경 세팅

---
+++ src/main/java/com/memo/Memo1Application.java
@@ -0,0 +1,13 @@
+package com.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Memo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Memo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/memo/Memo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Memo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
